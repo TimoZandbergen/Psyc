52b9a1b491dee4e68ffca3c3fce487435f6f2ed4
TimoZandbergen <zandbergentimo@gmail.com>
2022-11-22 17:52:58 +0100

    scene switching and checking which scene you are in

--- Psyc/Assets/Scripts/Seamless.cs
+++ Psyc/Assets/Scripts/Seamless.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -11,10 +12,15 @@ public class Seamless : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         _nextScene = true;
-    }
-
-    private void Update()
-    {
         if (_nextScene) SceneManager.LoadScene(1);
+        StartCoroutine(WaitingForSceneSwitch(10));
+        
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+        if (sceneName == "Scene City") SceneManager.LoadScene(2);
+    }
+    private IEnumerator WaitingForSceneSwitch(float waitingtime)
+    {
+        yield return new WaitForSeconds(waitingtime);
     }
 }
