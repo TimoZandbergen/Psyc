1326655d542f95400d112241f94c253be9201eb6
Damon-Jay <damon.jellema2004@gmail.com>
2022-12-13 13:05:33 +0100

    point walk enemy

---
+++ Psyc/Assets/Scripts/Ai.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ai : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    public float radius;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (!agent.hasPath)
+        {
+            agent.SetDestination(GetPoint.Instance.GetRandomPoint());
+        }
+    }
+
+#if UNITY_EDITOR
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+#endif
+}
